c12f933f17509a37d379d7094bcdc36b1222d832
Santiago <santiagorojas.plc@gmail.com>
2022-08-08 21:29:22 -0300

    redux to dev

---
+++ src/redux/actions.js
@@ -0,0 +1,91 @@
+import axios from "axios";
+import {  GET_TYPES ,URL_BASE , ORDER_ALPHA, ORDER_SCORE, FILTER_BY_TYPE } from "./constantes";
+
+
+
+export const loadTypes = () => {
+    return async (dispatch) => {
+        try {
+            const api = await axios.get(`${URL_BASE}/types`);
+            const res= api.data;
+            console.log("desde actions:");
+            return dispatch({
+                type: GET_TYPES,
+                payload: res,
+            })
+        }
+        catch (error) { console.log(error) }
+    }
+};
+
+export const getAllproyects = () => {
+    return async (dispatch) => {
+        try {
+            const api = await axios.get(`${URL_BASE}/proyects`);
+            const res= api.data;
+            console.log("desde actions:");
+            return dispatch({
+                type: GET_RECIPES,
+                payload: res,
+            })
+        }
+        catch (error) { console.log(error) }
+    }
+};
+
+
+
+
+
+export function postUser(payload) {
+    return async function(dispatch){
+        try{const respuesta= await axios.post("http://localhost:3001/recipe", payload)
+        console.log(respuesta);
+        return respuesta } 
+        catch(err){
+            console.log(err.massage);
+            return false
+        }      
+    }
+}
+
+export const orderAlpha = (way) => {
+    return async (dispatch) => {
+        
+        console.log(`accion ordenar alpha ${way}`);
+    
+        return dispatch({
+            type: ORDER_ALPHA,
+            payload:way,
+        
+        })
+        
+    }
+};
+export const orderScore = (way) => {
+    return async (dispatch) => {
+        
+        console.log(`accion ordenar score ${way}`);
+    
+        return dispatch({
+            type: ORDER_SCORE,
+            payload:way,
+        
+        })
+        
+    }
+};
+
+export const filterByType = (way) => {
+    return async (dispatch) => {
+        
+        console.log(`accion Filtrar x ${way}`);
+    
+        return dispatch({
+            type: FILTER_BY_TYPE,
+            payload:way,
+        
+        })
+        
+    }
+};
