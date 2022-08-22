80f029dcc4f714d8d79a718981734e7d8a88dbe0
Luciano-Olmedo <lucianonicolasolmedo@gmail.com>
2022-08-22 18:44:40 -0300

    Cards: Profile Details for students

---
+++ index/src/components/ProfileDetail/ProfileDetail.js
@@ -0,0 +1,41 @@
+import {Container,CardContainer,Imagen,Titulo,Texto,ListTech} from "./assets/Styles"
+import image1 from "./assets/image1.jpg"
+
+const ProfileDetail = () => {
+  return (
+    <Container>
+      <CardContainer >
+
+        <Imagen>
+          <img src={image1} alt="photo" />
+        </Imagen>
+        <Titulo>Luciano Olmedo</Titulo><br />
+        <Texto >Puesto: Frontend Developer</Texto><br />
+        <Texto >Tiempo: 9 meses</Texto><br />
+        <Texto style={{ color: "purple" }}>ver más</Texto><br />
+        <Texto>Tecnologías</Texto>
+        <ListTech>
+          <ul>
+            <li>*REACT</li>
+            <li>*NODEJS</li>
+            <li>*MONGODB</li>
+          </ul>
+
+        </ListTech>
+
+
+
+
+
+      </CardContainer>
+
+    </Container>
+  )
+}
+
+
+
+
+
+
+export default ProfileDetail
