e7b7949fc2db8a98e4848224afde28a31a94638c
Julián Perna <julianperna@gmail.com>
2022-08-25 20:42:54 -0300

    se creó el banner en modo desktop

--- index/src/App.js
+++ index/src/App.js
@@ -11,6 +11,7 @@ import Create from "./pages/Create/Create"
 import Header from "./components/Header/Header"
 import NavBarMobile from "./components/NavbarMobile/NavbarMobile"
 import ProfileDetail from "./components/ProfileDetail/ProfileDetail"
+import Main from "./components/Main/Main"
 
 function App() {
   const size = useWindowSize() //anchura y altura de la pantalla number[]
@@ -20,9 +21,8 @@ function App() {
   const location = useLocation()
 
   useEffect(() => {
-    if(!appRef.current) return
-    if(appRef.current.children[1].innerHTML.elementType !== "nav") return //solucion error que me agarraba el navbar y le aplicaba overflow
-
+    if (!appRef.current) return
+    if (appRef.current.children[1].innerHTML.elementType !== "nav") return //solucion error que me agarraba el navbar y le aplicaba overflow
     appRef.current.children[1].style.overflowY = "auto" // esto es para que el navbar siempre quede abajo en la pantalla
     appRef.current.children[1].style.flexGrow = "1"
   }, [size])
@@ -31,24 +31,24 @@ function App() {
     dispatch(setNewSize(size)) // cada vez que cambien las dimensiones de la pantalla queremos tenerlo disponible para cualquier componente que lo necesite
   }, [size])
 
-  useEffect(()=>{
+  useEffect(() => {
     location.pathname === "/" && navigate("search/projects")
-  },[])
+  }, [])
 
   return (
     <div className="App" ref={appRef}>
-      <Header /> 
+      <Header />
+      <Main />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/create" element={<Create />} />
         <Route path="/search/:articles" element={<Search />}></Route>
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/create" element={<Create/>} />
-        <Route path='/search/:articles' element={<Search/>}>
-        </Route> 
-        <Route path="search/projects/:idProject" element={<ProfileDetail/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/search/:articles" element={<Search />}></Route>
+        <Route path="search/projects/:idProject" element={<ProfileDetail />} />
       </Routes>
       <NavBarMobile />
     </div>
