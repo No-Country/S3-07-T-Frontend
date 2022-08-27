add81884c61b43d19ba22eaf35a216ea409bdf69
Santiago <santiagorojas.plc@gmail.com>
2022-08-26 22:16:14 -0300

    url

--- index/src/configs/URLS.js
+++ index/src/configs/URLS.js
@@ -1,5 +1,4 @@
 const BASE_URL = process.env.REACT_APP_ENV === "development" ? "http://localhost:3002/api" : "https://nc-community.herokuapp.com/api"
-// const BASE_URL="https://nc-community.herokuapp.com/api"
 
 export const SERVER_URLS = {
   LOGIN: `${BASE_URL}/signIn`,
