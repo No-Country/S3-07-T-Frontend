c06cb76148b125bb54d2ca648deb100cf0683f2a
Santiago <santiagorojas.plc@gmail.com>
2022-08-27 12:02:06 -0300

    detail #3

--- index/src/pages/detail/proyectDetail.js
+++ index/src/pages/detail/proyectDetail.js
@@ -24,11 +24,13 @@ export default function ProyectDetail(){
 
   const MyButton=styled.button`
     padding:1em;
-    margin:3em;
-    background:#7babc5;
-    color: #fff;
+    margin: 3em;
+    border: 3px #44b3e9 solid;
+    background:#090909;
+    color: #62f3d5;
     font-weight: bold;
     font-style: italic;
+    z-index: 2;
     `
   const MyImg=styled.img`
     width: 60%;
@@ -38,14 +40,16 @@ export default function ProyectDetail(){
   const Detalles=styled.div`
     display: grid;
     grid-template-columns: repeat(auto-fit,minmax(360px,2fr));
-    color: #fff;
+    color: #6c2de3;
     text-align: center;
+    background: linear-gradient(to left, #333, transparent 50%, #fff 75%, #333 75%);
     `
-  // const Div2=styled.div`
-  //   opacity:${mas?"1":"0"};
-  //   `
+  const Div2=styled.div`
+  opacity:${mas?"1":"0"};
+  `
   const Div1=styled.div`
     margin-top: 4%;
+    z-index: 2;
     `
   while(!proyect){
     return(
@@ -55,13 +59,31 @@ export default function ProyectDetail(){
   return( 
     <Detalles>
       <Div1>
+        <p>{proyect._id}</p>
         <MyImg src={proyect.image}  alt={"otra imagen"}  /> <br/>
-        <p>{proyect.description}</p>
+        <p style={{color:"#44b3e9"}}>{proyect.title}</p>
         <MyButton onClick={handleClick}>Ver más</MyButton>  
       </Div1>
-      {/* <Div2 >
+      <Div2 >
+        <p>{proyect.description}</p>
+        <video
+          controls
+          loop
+          muted
+          style={{
+            position: "absolute",
+            left:"40%",
+            width: "56%",
+            height: "50vh",
+            objectFit: "cover",
+            zIndex: 1,
+            transform:"traslate(40%, 30%)",
+          }}
+        >
+          <source src={proyect.video} type="video/mp4" />
+        </video>
         <MyButton onClick={handleClick}>Ocultar</MyButton> <br/>
-      </Div2> */}
+      </Div2> 
     </Detalles>
   )
 }
