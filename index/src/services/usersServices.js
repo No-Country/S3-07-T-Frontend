bf4784992c59a58d0e1b7418b66b8cd764d34ca2
EnzoGRDev <enzo.g.rz.18@gmail.com>
2022-08-31 15:41:25 -0300

    ruta registro ahora tira de servidor dinamico

--- index/src/services/usersServices.js
+++ index/src/services/usersServices.js
@@ -2,6 +2,22 @@ import axios from "axios"
 import { SERVER_URLS } from "../configs/URLS"
 import { requestError } from "./errorServices"
 
+export const login = async (data) => {
+  try {
+    const res = await axios.post(SERVER_URLS.LOGIN, data)
+    if(res.status === 200){
+      return res.data
+    } else throw res
+  } catch (error) {
+    return Promise.reject(error)
+  }
+}
+
+export const register = async (data) => {
+  const res = await axios.post(SERVER_URLS.REGISTER, data)
+  return res.data
+}
+
 export const getUserByID = async (id = "6302f80e05c3ebcc65a24889") => {
   try {
     const res = await axios.get(`${SERVER_URLS.GETUSERID}/${id}`)
