e11ef97a04acfe51bac01e263696538814e8fe45
EnzoGRDev <enzo.g.rz.18@gmail.com>
2022-09-02 19:04:39 -0300

    profile detail conectado a api

--- index/src/components/ProfileDetail/ProfileDetail.js
+++ index/src/components/ProfileDetail/ProfileDetail.js
@@ -1,33 +1,77 @@
-import {Container,CardContainer,Imagen,Titulo,Texto,ListTech} from "./assets/Styles"
+import { Container, CardContainer, Titulo, Texto } from "./assets/Styles"
 import image1 from "./assets/image1.jpg"
 import { Helmet } from "react-helmet"
+import { useState } from "react"
+import { useEffect } from "react"
+import { getUserByID } from "../../services/usersServices"
+import { useParams } from "react-router-dom"
+import { ContainerVideoAndInfo, SectionTagsInDetail } from "../../pages/ProjectDetail/ProjectDetailStyles"
+import { categoriesSearchsTypes } from "../../Types/articles_type"
+import { adapterToTagInSlider } from "../MyProfile/MyProfile"
+import SliderTags from "../SliderTags/SliderTags"
+import { getListTech } from "../../services/techsServices"
+import { Avatar } from "../MyProfile/MyProfileStyles"
+
+const profileType = {
+  "id": "",
+  "firstName": "",
+  "lastName": "",
+  "rolDes": "",
+  "phone": "",
+  "email": "",
+  "projects": [],
+  "teams": []
+}
 
 const ProfileDetail = () => {
+  const [profile, setProfile] = useState(profileType)
+  const [technologies, setTechnologies] = useState([])
+  const { idProfile } = useParams()
+
+  useEffect(() => {
+    getUserByID(idProfile)
+      .then((res) => {
+        console.log(res)
+        setProfile(res)
+      })
+      .catch((error) => console.log(error))
+
+    getListTech()
+      .then(setTechnologies)
+      .catch((error) => console.log(error))
+  }, [idProfile])
+
   return (
     <Container>
       <Helmet>
-        <title> Perfil | NC community </title>
-        
+        <title> Perfil | {`${profile.firstName} ${profile.lastName}` || "Perfil de NC"} </title>
       </Helmet>
       <CardContainer >
 
-        <Imagen>
-          <img src={image1} alt="photo" />
-        </Imagen>
-        <Titulo>Luciano Olmedo</Titulo><br />
-        <Texto >Puesto: Frontend Developer</Texto><br />
-        <Texto >Tiempo: 9 meses</Texto><br />
-        <Texto style={{ color: "purple" }}>ver más</Texto><br />
-        <Texto>Tecnologías</Texto>
-        <ListTech>
-          <ul>
-            <li>*REACT</li>
-            <li>*NODEJS</li>
-            <li>*MONGODB</li>
-          </ul>
+        <ContainerVideoAndInfo>
+          <Avatar src={profile.avatar || image1} alt="photo" />
+          <Titulo>{profile.firstName} {profile.lastName}</Titulo>
+          <Texto >Puesto: {profile.rolDes} </Texto>
+          <Texto >Tiempo: 9 meses</Texto>
+          <Texto style={{ color: "purple" }}>ver más</Texto>
+        </ContainerVideoAndInfo>
+        <SectionTagsInDetail>
+          <h2>Tecnologías</h2>
+          {
+            technologies.length ? 
+              <SliderTags tags={adapterToTagInSlider(technologies, categoriesSearchsTypes.technology)}/>
+              : <p>No hay tecnologías descriptas aún sobre este perfil</p>
+          }
+        </SectionTagsInDetail>
 
-        </ListTech>
-
+        <SectionTagsInDetail>
+          <h2>Proyectos</h2>
+          {
+            profile.projects.length ? 
+              <SliderTags tags={adapterToTagInSlider(profile.projects, categoriesSearchsTypes.project)}/>
+              : <p>No hay proyectos descriptas aún sobre este perfil</p>
+          }
+        </SectionTagsInDetail>
 
 
 
